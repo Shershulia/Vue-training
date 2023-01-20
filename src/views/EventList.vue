067f7d08b130279d98362d9880237611cb6d8199
Ivan Shirshikov <ivansh@stud.ntnu.no>
2023-01-20 16:36:00 +0100

    Pagination using dynamic routing

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,6 +2,19 @@
   <div class="events">
     <h1>List of events:</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <router-link
+      :to="{ name: 'EventList', query: { page: page - 1 } }"
+      rel="prev"
+      v-if="page != 1"
+      >Last</router-link
+    >
+
+    <router-link
+      :to="{ name: 'EventList', query: { page: page + 1 } }"
+      rel="next"
+      v-if="hasNextPage"
+      >Next</router-link
+    >
   </div>
 </template>
 
@@ -9,22 +22,36 @@
 // @ is an alias to /src
 import EventCard from "@/components/EventCard.vue"
 import EventService from "@/services/EventService"
+import { watchEffect } from "vue"
+
 export default {
   name: "EventList",
+  props: ["page"],
   components: {
     EventCard,
   },
   data() {
     return {
       events: null,
+      total: 0,
     }
   },
   created() {
-    EventService.getEvents()
-      .then((response) => {
-        this.events = response.data
-      })
-      .catch((error) => console.log(error))
+    watchEffect(() => {
+      this.events = null
+      EventService.getEvents(2, this.page)
+        .then((response) => {
+          this.events = response.data
+          this.total = response.headers["x-total-count"]
+        })
+        .catch((error) => console.log(error))
+    })
+  },
+  computed: {
+    hasNextPage() {
+      var totalPages = Math.ceil(this.total / 2)
+      return this.page < totalPages
+    },
   },
 }
 </script>
